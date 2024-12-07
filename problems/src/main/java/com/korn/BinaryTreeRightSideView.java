468cf508a2b9a8f15a773a51fe991fbdf2961cbe
AndreiKorn <a.kornienkov94@gmail.com>
2024-12-07 10:49:20 +0100

    Added solution for "Binary Tree Right Side View"

---
+++ problems/src/main/java/com/korn/BinaryTreeRightSideView.java
@@ -0,0 +1,42 @@
+package com.korn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeRightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        addRightmost(result, root, 0);
+
+        return result;
+    }
+
+    private void addRightmost(List<Integer> result, TreeNode node, int currentDepth) {
+        if (node == null) {
+            return;
+        }
+
+        if (currentDepth >= result.size()) {
+            result.add(node.val);
+        }
+
+        addRightmost(result, node.right, currentDepth + 1);
+        addRightmost(result, node.left, currentDepth + 1);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ problems/src/test/java/com/korn/BinaryTreeRightSideViewTest.java
@@ -0,0 +1,48 @@
+package com.korn;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import com.korn.BinaryTreeRightSideView.TreeNode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BinaryTreeRightSideViewTest {
+    @ParameterizedTest
+    @MethodSource
+    void rightSideView(TreeNode root, List<Integer> expectedResult) {
+        List<Integer> actualResult = new BinaryTreeRightSideView().rightSideView(root);
+
+        assertThat(actualResult)
+                .containsExactlyInAnyOrderElementsOf(expectedResult);
+    }
+
+    static Stream<Arguments> rightSideView() {
+        return Stream.of(
+                Arguments.of(null, List.of()),
+                Arguments.of(new TreeNode(1), List.of(1)),
+                Arguments.of(new TreeNode(1, new TreeNode(2), new TreeNode(3)), List.of(1, 3)),
+                Arguments.of(new TreeNode(1, new TreeNode(2, new TreeNode(4), null), null), List.of(1, 2, 4)),
+                Arguments.of(new TreeNode(1, null, new TreeNode(3, null, new TreeNode(5))), List.of(1, 3, 5)),
+                Arguments.of(
+                        new TreeNode(
+                                1,
+                                new TreeNode(
+                                        2,
+                                        new TreeNode(
+                                                4,
+                                                new TreeNode(6),
+                                                null
+                                        ),
+                                        new TreeNode(5)
+                                ),
+                                new TreeNode(3)
+                        ),
+                        List.of(1, 3, 5, 6)
+                )
+        );
+    }
+}
