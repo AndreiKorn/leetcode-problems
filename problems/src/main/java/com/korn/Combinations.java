29732037cd57a48a75c0f89eb664ddc68da33500
AndreiKorn <a.kornienkov94@gmail.com>
2024-12-12 09:23:53 +0100

    Added solution for "Combinations"

---
+++ problems/src/main/java/com/korn/Combinations.java
@@ -0,0 +1,37 @@
+package com.korn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), n, k, 1);
+        return result;
+    }
+
+    private void backtrack(
+            List<List<Integer>> result,
+            List<Integer> intermediateResult,
+            int n,
+            int k,
+            int startingValue
+    ) {
+        if (intermediateResult.size() == k) {
+            result.add(new ArrayList<>(intermediateResult));
+            return;
+        }
+        
+        int need = k - intermediateResult.size();
+        int left = n - startingValue + 1;
+        if (need > left) {
+            return;
+        }
+
+        for (int i = startingValue; i <= n; i++) {
+            intermediateResult.add(i);
+            backtrack(result, intermediateResult, n, k, i + 1);
+            intermediateResult.remove(intermediateResult.size() - 1);
+        }
+    }
+}
---
+++ problems/src/test/java/com/korn/CombinationsTest.java
@@ -0,0 +1,73 @@
+package com.korn;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CombinationsTest {
+    @ParameterizedTest
+    @MethodSource
+    void combine(int n, int k, List<List<Integer>> expectedResult) {
+        List<List<Integer>> actualResult = new Combinations().combine(n, k);
+
+        assertThat(actualResult)
+                .containsExactlyInAnyOrderElementsOf(expectedResult);
+    }
+
+    static Stream<Arguments> combine() {
+        return Stream.of(
+                Arguments.of(1, 1, List.of(List.of(1))),
+                Arguments.of(2, 1, List.of(List.of(1), List.of(2))),
+                Arguments.of(2, 2, List.of(List.of(1, 2))),
+                Arguments.of(
+                        4,
+                        2,
+                        List.of(
+                                List.of(1, 2),
+                                List.of(1, 3),
+                                List.of(1, 4),
+                                List.of(2, 3),
+                                List.of(2, 4),
+                                List.of(3, 4)
+                        )
+                ),
+                Arguments.of(4, 4, List.of(List.of(1, 2, 3, 4))),
+                Arguments.of(
+                        20,
+                        19,
+                        List.of(
+                                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19),
+                                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20),
+                                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19, 20),
+                                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 20),
+                                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20),
+                                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20),
+                                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19, 20),
+                                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 17, 18, 19, 20),
+                                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20),
+                                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+                                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+                                List.of(1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+                                List.of(1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+                                List.of(1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+                                List.of(1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+                                List.of(1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+                                List.of(1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+                                List.of(1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+                                List.of(1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20),
+                                List.of(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+                        )
+                ),
+                Arguments.of(
+                        20,
+                        20,
+                        List.of(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20))
+                )
+        );
+    }
+}
