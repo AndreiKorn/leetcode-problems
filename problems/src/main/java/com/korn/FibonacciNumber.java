dd084aeff5eed2af36460a41899a8c41ca4b799b
AndreiKorn <a.kornienkov94@gmail.com>
2024-12-12 09:30:14 +0100

    Updated solution for "Fibonacci Number"

--- problems/src/main/java/com/korn/FibonacciNumber.java
+++ problems/src/main/java/com/korn/FibonacciNumber.java
@@ -4,13 +4,11 @@ public class FibonacciNumber {
     public int fib(int n) {
         if (n == 0 || n == 1) {
             return n;
-        } else if (n == 2) {
-            return 1;
         }
 
-        int beforePrevious = 1;
+        int beforePrevious = 0;
         int previous = 1;
-        for (int i = 3; i < n; i++) {
+        for (int i = 2; i < n; i++) {
             int next = beforePrevious + previous;
             beforePrevious = previous;
             previous = next;
