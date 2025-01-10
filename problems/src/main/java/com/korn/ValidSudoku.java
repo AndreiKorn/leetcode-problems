fd91f98c5b1b682c84d1bec075495b9745183692
AndreiKorn <a.kornienkov94@gmail.com>
2025-01-10 15:05:01 +0100

    Updates solution for "Valid Sudoku"

--- problems/src/main/java/com/korn/ValidSudoku.java
+++ problems/src/main/java/com/korn/ValidSudoku.java
@@ -5,9 +5,9 @@ package com.korn;
  */
 public class ValidSudoku {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] rows = new boolean[9][9];
-        boolean[][] columns = new boolean[9][9];
-        boolean[][] subBoxes = new boolean[9][9];
+        int[] rowsMasks = new int[9];
+        int[] columnMasks = new int[9];
+        int[] subBoxesMasks = new int[9];
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
@@ -15,24 +15,22 @@ public class ValidSudoku {
                     continue;
                 }
 
-                if (rows[i][board[i][j] - '1']) {
+                int cellMask = 1 << (board[i][j] - '1');
+                if ((rowsMasks[i] & cellMask) != 0) {
                     return false;
-                } else {
-                    rows[i][board[i][j] - '1'] = true;
                 }
+                rowsMasks[i] |= cellMask;
 
-                if (columns[j][board[i][j] - '1']) {
+                if ((columnMasks[j] & cellMask) != 0) {
                     return false;
-                } else {
-                    columns[j][board[i][j] - '1'] = true;
                 }
+                columnMasks[j] |= cellMask;
 
                 int subBoxIndex = (i / 3) * 3 + (j / 3);
-                if (subBoxes[subBoxIndex][board[i][j] - '1']) {
+                if ((subBoxesMasks[subBoxIndex] & cellMask) != 0) {
                     return false;
-                } else {
-                    subBoxes[subBoxIndex][board[i][j] - '1'] = true;
                 }
+                subBoxesMasks[subBoxIndex] |= cellMask;
             }
         }
 
