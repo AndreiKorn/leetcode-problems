c58190de7c699c86ea4c463cb6244a28915c4643
AndreiKorn <a.kornienkov94@gmail.com>
2025-02-16 10:47:32 +0100

    Enhanced solution for "Add Two Numbers"

--- problems/src/main/java/com/korn/AddTwoNumbers.java
+++ problems/src/main/java/com/korn/AddTwoNumbers.java
@@ -10,33 +10,13 @@ public class AddTwoNumbers {
         ListNode dummy = new ListNode();
         ListNode current = dummy;
         boolean overflow = false;
-        while (l1 != null && l2 != null) {
-            int tempSum = l1.val + l2.val + (overflow ? 1 : 0);
+        while (l1 != null || l2 != null || overflow) {
+            int tempSum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + (overflow ? 1 : 0);
             current.next = new ListNode(tempSum % 10);
             current = current.next;
             overflow = tempSum > 9;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-
-        while (l1 != null) {
-            int tempSum = l1.val + (overflow ? 1 : 0);
-            current.next = new ListNode(tempSum % 10);
-            current = current.next;
-            overflow = tempSum > 9;
-            l1 = l1.next;
-        }
-
-        while (l2 != null) {
-            int tempSum = l2.val + (overflow ? 1 : 0);
-            current.next = new ListNode(tempSum % 10);
-            current = current.next;
-            overflow = tempSum > 9;
-            l2 = l2.next;
-        }
-
-        if (overflow) {
-            current.next = new ListNode(1);
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
         }
 
         return dummy.next;
